0f3e46cb45867f808290dd11799d9eefbcc74047
Hiroki Kato <hkato@sas.upenn.edu>
2018-05-08 18:05:01 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ require 'pry'
 
 def reverse_each_word(string)
 # binding.pry
-  string.split.collect {|word| word.reverse.join(" ")}
+  string.split.collect {|word| word.reverse}.join(" ")
   end
 end
